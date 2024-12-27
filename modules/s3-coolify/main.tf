33bca75182e011348f72fb87ce1e74ecc09101c6
Gabriel Melo <melogoncalvesbiel@gmail.com>
2024-12-27 13:50:40 -0300

    feat: s3 coolify

---
+++ modules/s3-coolify/main.tf
@@ -0,0 +1,15 @@
+resource "aws_s3_bucket" "coolify_bucket" {
+  bucket = "coolify"
+}
+
+output "bucket" {
+  value = aws_s3_bucket.coolify_bucket.bucket
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.coolify_bucket.bucket_domain_name
+}
+
+output "bucket_endpoint" {
+  value = aws_s3_bucket.coolify_bucket.website_endpoint
+}
