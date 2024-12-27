e700c5bfd2f0e260d90b117e2bcd5ea1b2adbd9a
Gabriel Melo <melogoncalvesbiel@gmail.com>
2024-12-27 11:23:45 -0300

    feat: output public ip

--- main.tf
+++ main.tf
@@ -2,11 +2,15 @@ provider "aws" {
   region = var.region
 }
 
-module "vpc_playground" {
+module "ec2_playground" {
   source = "./modules/ec2-playground"
   ami_id      = "ami-0e2c8caa4b6378d8c"
   key_name    = var.key_name
   public_key  = var.public_key
   availability_zone = var.availability_zone
+}
+
+output "public_ip" {
+  value = aws_instance.ec2_instance.public_ip
 }
 
